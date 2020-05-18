62e934cb1d8b1ff26f32d94b905919b819493814
NKStupid <64253633+NKStupid@users.noreply.github.com>
2020-05-18 19:27:11 +0800

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,12 +1,38 @@
-﻿using System;
+using System;
+using System.Threading.Tasks;
+using SendGrid;
+using SendGrid.Helpers.Mail;
 
-namespace SendGridDemo
+namespace ConsoleApp1
 {
     class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+	        SendMail().Wait();
+	        Console.WriteLine("Sent Successfully!!--in the main func");
+        }
+
+        static async Task SendMail()
+        {
+            var apiKey = "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";
+            var client = new SendGridClient(apiKey);
+            var msg = new SendGridMessage()
+            {
+                From = new EmailAddress("test@example.com", "DXJoseph Team"),
+                Subject = "Hello World from the SendGrid CSharp SDK!",
+                PlainTextContent = "Hello, Email!",
+                HtmlContent = "<strong>Hello, Email!</strong>"
+            };
+            // msg.AddTo(new EmailAddress("r-arai@nri.co.jp", "Test User"));
+            
+            msg.AddTo(new EmailAddress("joseph.siyi@gmail.com", "Test User"));
+            msg.AddTo(new EmailAddress("chenkiegcp1@gmail.com", "Test User"));
+            var response = await client.SendEmailAsync(msg);
+
+	        Console.WriteLine("Sent Successfully!!--in the async func");
+	        Console.WriteLine(response.ToString());
         }
     }
 }
